142cb8803cd27cbf71c1cea63b608dba0a6ef5ad
Chandru-Ghub <chandruinfo455@gmail.com>
2023-07-27 15:30:27 +0530

    renewed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,15 +16,15 @@ const Cart = () => {
   return (
     <div className='cartContainer'>
       <div className='cartnav'>
-      <h2 className='cartTitle' style={{fontSize:'30px',color:'black'}}>icart</h2>
-      {cart.length?(<h3 className='totalPrice' style={{color:'blue'}}>Total Price: ₹{count+pro}🪙 </h3>):null}
-      
-      {cart.length?(
-      <button className='removeAll' onClick={()=>{
-        if(window.confirm('Warning! all items inside the card will be removed'))
-        setCart([])
-      }}>Remove All</button>):null}
-        <p className='qty'>PerItems: ₹{pro}</p>
+            <h2 className='cartTitle' style={{fontSize:'30px',color:'black'}}>icart</h2>
+            {cart.length?(<h3 className='totalPrice' style={{color:'blue'}}>Total Price: ₹{count+pro}🪙 </h3>):null}
+            
+            {cart.length?(
+            <button className='removeAll' onClick={()=>{
+              if(window.confirm('Warning! all items inside the card will be removed'))
+              setCart([])
+            }}>Remove All</button>):null}
+              <p className='qty'>PerItems: ₹{pro}</p>
       </div>
       {cart.length<1? (<span className='empty'>
         <img className='kart' src="https://kalpp.com/img/empty-cart.05d8cea1.gif" alt="emptyCart" />
@@ -33,11 +33,12 @@ const Cart = () => {
      ):null}
     
       <div className='cart-card'>
-        {cart.map((prod)=>(
+        {cart.map((prod,i)=>(
           <Cartproducts
           prod={prod}
           pro = {pro}
           setpro = {setpro}
+          key={i}
          />
         ))}
        
